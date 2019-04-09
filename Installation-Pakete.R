de2d34add1cd952aa32033a5f6f05f8329b12248
Tom Gerlach <tomgerlach@MBP-Tom.local>
2019-04-09 12:38:58 +0200

    Mehreres

---
+++ Installation-Pakete.R
@@ -0,0 +1,6 @@
+install.packages("knitr", dependencies = TRUE)
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("ggplot", dependencies = TRUE)
+install.packages("kableExtra", dependencies = TRUE)
+install.packages("ggplot2", dependencies = TRUE)
+
